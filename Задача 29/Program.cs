ffa59b4d456cee760d99a4f1fc172b7975f84b3b
Anna Golikova <annagolikovast@gmail.com>
2022-09-04 09:46:55 +0400

    29 done

---
+++ Задача 29/Program.cs
@@ -0,0 +1,16 @@
+﻿void randomArray(int number, int min, int max)
+
+{
+    int[] array = new int[number];
+
+    for (int i = 0; i < number; i++)
+    {
+        array[i] = new Random().Next(min, (max + 1));
+        Console.Write(array[i]+" ");
+    }
+
+}
+
+
+randomArray(5, 1, 20);
+
