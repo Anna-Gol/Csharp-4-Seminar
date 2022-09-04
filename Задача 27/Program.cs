3b7bf1d82fb55a2dcce1213f9cdc69f537845461
Anna Golikova <annagolikovast@gmail.com>
2022-09-04 07:20:48 +0400

    27 done

---
+++ Задача 27/Program.cs
@@ -0,0 +1,17 @@
+﻿int digSum (int number)
+
+{
+
+int sum = 0;
+while (number != 0)
+{
+    sum += number % 10;
+    number /= 10;
+}
+
+    return sum;
+
+}
+
+int dSum = digSum (9012);
+Console.WriteLine ($"Сумма цифр в числе = {dSum}");
